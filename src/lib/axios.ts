7c4543afceb56aa06d2a6891b44bfde0c68f4dfd
annansadr22 <annansadr.ca@gmail.com>
2025-05-14 17:38:44 +0530

    Replaces axios URL with correct URL

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 
 // Create an Axios instance
 const api = axios.create({
-  baseURL: "http://127.0.0.1:8000/", // <-- Make sure this is correct
+  baseURL: "https://cabin-booking-backend.onrender.com/", // <-- Make sure this is correct
   withCredentials: false,
   headers: {
     "Content-Type": "application/json",
